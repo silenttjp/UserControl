4efc9262a30615d4f4982067f9a270388d0e3b29
tj <silenttjp@gmail.com>
2014-11-14 01:59:59 -0500

    Making sure models are loading properly

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace IDG\UserControlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use IDG\UserControlBundle\Models\Lists;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        $lists = new Lists();
         return $this->render('IDGUserControlBundle:Default:index.html.twig', array('name' => $name));
     }
 }
