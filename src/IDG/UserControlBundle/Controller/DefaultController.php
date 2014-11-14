edb6acc02ae035dd0c4ba4e0c0f853a52a99f0e1
tj <silenttjp@gmail.com>
2014-11-14 16:32:32 -0500

    ...

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $Users = UserQuery::create()->find();
-        print_r($Users);
-        return $this->render('IDGUserControlBundle:User:user.html.twig', array('name' => $name));
+        print_r($Users->ToArray());
+        return $this->render('IDGUserControlBundle:User:user.html.twig');
     }
 }
