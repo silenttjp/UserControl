2444fb5bff605f111d4b2ad1a9f84d3a7195f372
tj <silenttjp@gmail.com>
2014-11-14 23:00:55 -0500

    Adding ajax

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace IDG\UserControlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use IDG\UserControlBundle\Models\UserQuery;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,4 +15,19 @@ class DefaultController extends Controller
         return $this->render('IDGUserControlBundle:User:user.html.twig', array(  'user_count' => count($Users)));
     }
     
+    public function getUsersAction(){
+       
+        $Users = UserQuery::create()->find();
+         
+        if(count($Users)){
+            $message = "No Users";
+            $return=array("responseCode"=>400,  "message"=>$message);
+        }
+        else{
+            $return=array("responseCode"=>400, "users"=>$Users->ToArray());
+        }
+    
+        $return=json_encode($return);
+        return new Response($return,200,array('Content-Type'=>'application/json'));
+    }
 }
