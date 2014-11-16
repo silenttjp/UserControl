553b41ac3d538dbb96fe824f0a5211bda174f57a
tj <silenttjp@gmail.com>
2014-11-15 22:37:37 -0500

    ...

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -41,7 +41,7 @@ class DefaultController extends Controller
         $user_form = $this->createForm(new UserType(), $user);
     
         $user_form->handleRequest($request);
-        print_r($user_form->all());
+        print_r($user_form->getData());
         //$validator = $this->get('validator');
        // $errors = $validator->validate($author);
     
