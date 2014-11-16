44a5ffcfc51a1b8d7c33f4c1ccb26a337c3bb4be
tj <silenttjp@gmail.com>
2014-11-16 17:25:52 -0500

    ...

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -57,7 +57,7 @@ class DefaultController extends Controller
         $user->delete();
         
         
-       $this->redirect($this->generateUrl('user_bundle_routing'));
+       $this->forward('Default','getUsers');
     }
     
     
