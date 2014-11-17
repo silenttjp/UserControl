0027b740e44f8ecd93da3f8f026c14523cee22a3
tj <silenttjp@gmail.com>
2014-11-16 22:15:29 -0500

    fix save user

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -68,7 +68,7 @@ class DefaultController extends Controller
 
             $request = $this->getRequest();
             
-            if(empty($request->request->get('id'))){
+            if(!empty($request->request->get('user_id'))){
                 $user = UserQuery::create()->findPk($request->request->get('user_id'));
             } else {
                 $user = new User();
@@ -88,7 +88,7 @@ class DefaultController extends Controller
         $request = $this->getRequest();
     
         if(empty($request->request->get('id'))){
-            $lists = ListsQuery::create()->findPk($request->request->get('lists_id'));
+            $lists = ListsQuery::create()->findPk($request->request->get('list_id'));
         } else {
             $lists = new Lists();
         }
