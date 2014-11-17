9e8aa2fbf83808b5896726b0fa768361c4a15d33
tj <silenttjp@gmail.com>
2014-11-16 20:35:50 -0500

    fix

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -16,7 +16,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $Users = UserQuery::create()->fin();
+        $Users = UserQuery::create()->find();
 
         return $this->render('IDGUserControlBundle:User:user.html.twig', array(  'user_count' => count($Users)));
     }
