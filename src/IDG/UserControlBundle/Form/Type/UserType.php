bae7250d0642e4e98fe7a1d02ecceaa0a4d59c6c
tj <silenttjp@gmail.com>
2014-11-16 19:03:18 -0500

    add id field

--- src/IDG/UserControlBundle/Form/Type/UserType.php
+++ src/IDG/UserControlBundle/Form/Type/UserType.php
@@ -19,7 +19,7 @@ class UserType extends BaseAbstractType
     {
         $builder->add('firstName','text', array("required"=>true));
         $builder->add('lastName','text', array("required"=>true));
-        $builder->add('id','text', array("hidden"=>true));
+        $builder->add('id','hidden');
         
         
     }
