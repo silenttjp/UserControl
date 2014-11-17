eb502f2e048efefeca432ac0950d96c6ea883336
tj <silenttjp@gmail.com>
2014-11-16 23:28:43 -0500

    ...

--- src/IDG/UserControlBundle/Models/User.php
+++ src/IDG/UserControlBundle/Models/User.php
@@ -3,10 +3,11 @@
 namespace IDG\UserControlBundle\Models;
 
 use IDG\UserControlBundle\Models\om\BaseUser;
+use IDG\UserControlBundle\Models\ListsQuery;
 
 class User extends BaseUser
 {
     public function getLists(){
-        
+        return ListsQuery::create()->find();
     }
 }
