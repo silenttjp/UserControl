aaabdda819baadb268308d00fc416ac094b7a357
tj <silenttjp@gmail.com>
2014-11-16 23:18:10 -0500

    ...

--- src/IDG/UserControlBundle/Form/Type/UserType.php
+++ src/IDG/UserControlBundle/Form/Type/UserType.php
@@ -25,9 +25,12 @@ class UserType extends BaseAbstractType
         $builder->add('lastName','text', array("required"=>true));
         $builder->add('id','hidden');
 
-        /*$builder->add('Lists', 'collection', array(
+        $builder->add('Lists', 'model', array(
             'type' => new ListsType()
-        ));*/
+        ));
+        
+        
+        
         
     }
     
