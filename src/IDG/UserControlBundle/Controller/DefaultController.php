96f2c0b65bde47b14ec848145af9e48f749f4895
tj <silenttjp@gmail.com>
2014-11-16 22:14:01 -0500

    fixing

--- src/IDG/UserControlBundle/Controller/DefaultController.php
+++ src/IDG/UserControlBundle/Controller/DefaultController.php
@@ -93,7 +93,7 @@ class DefaultController extends Controller
             $lists = new Lists();
         }
     
-        $lists->setName($request->request->get('lists_name'));
+        $lists->setName($request->request->get('list_name'));
         $lists->setActive('1');
         $lists->save();
     
